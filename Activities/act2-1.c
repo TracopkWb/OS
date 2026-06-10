4b1d4109786f9f2a65123e20129c71db46835900
Tracopk ZTP <tracopk.wb@gmail.com>
2026-06-10 00:38:01 -0600

    New work

---
+++ Activities/act2-1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+// #TITLE#
+
+// Functions Section
+
+// Global Variables
+
+// Structure Section
+
+int main()
+{
+    // Starts the process (parent)
+    int itsID = getpid();
+    printf("I am the parent, PID = %d\n", itsID);
+
+    int numberOfChildren = 0;
+    // Creation of the children
+    for (int i = 0; i < 3; i++)
+    {
+        pid_t pid = fork();
+
+        if (pid == 0)
+        {
+            printf("I am child %d, PID = %d\n", i + 1, getpid());
+            return 0;
+        }
+        else if (pid > 0)
+        {
+            numberOfChildren++;
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        wait(NULL);
+    }
+
+    printf("The parent with id %d, has %d children\n",itsID, numberOfChildren);
+    return 0;
+}
