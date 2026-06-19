fab61a38b5a579bc9118246b37757dbc0deb8c97
2173003062 <tracopk.wb@gmail.com>
2026-06-19 10:36:51 -0600

    Class

---
+++ Activities/Act4-1/queue-reader1.c
@@ -0,0 +1,75 @@
+// Standard C
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// File System & Permissions
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h> // FIFO pipes
+
+// System V IPC
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+#include "../../myLib/logger.h"
+
+// Activity 4 - Queue messages
+
+// Functions Section
+
+// Global Variables
+#define MAX_LENGTH 100
+
+// Structure Section
+struct message{
+    char text[MAX_LENGTH];
+    int type;
+};
+
+int main()
+{
+    struct message receivedMember;
+    struct message members[100];
+    struct msqid_ds queueInfo;
+
+    LOG_INFO("Starting Activity 4 - Queue message reader");
+
+    LOG_ACTION("Creating message queue");
+    key_t queueHashKey = ftok(".", 'A');
+
+    LOG_ACTION("Opening message queue");
+    int queueId = msgget(
+        queueHashKey,
+        IPC_CREAT | 0666);
+
+    if (queueId == -1)
+    {
+        LOG_ERROR("Failed to open message queue");
+        exit(EXIT_FAILURE);
+    }
+
+    LOG_SUCCESS("Message queue opened with id %d", queueId);
+
+    LOG_ACTION("Getting data from message queue");
+
+    while (1)
+    {
+        if (msgrcv(queueId,
+           &receivedMember,
+           sizeof(receivedMember) - sizeof(long),
+           0,
+           0) == -1)
+        {
+            LOG_ERROR("msgrcv failed");
+            exit(EXIT_FAILURE);
+        }
+
+        LOG_INFO("Received: %s, Level: %d",
+                 receivedMember.text,
+                 receivedMember.type);
+    }
+
+    return 0;
+}
---
+++ Activities/Act4-1/queue-writer.c
@@ -0,0 +1,73 @@
+//Standard C
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+//File System & Permissions
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h> // FIFO pipes
+
+// System V IPC 
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+#include "../../myLib/logger.h"
+
+
+// Activity 4 - Queue messages
+
+// Functions Section
+
+// Global Variables
+#define MAX_LENGTH 100
+
+// Structure Section
+struct message{
+    long type;
+    char text[MAX_LENGTH];
+};
+ 
+int main()
+{
+    LOG_INFO("Starting Activity 4 - Queue message writer");
+    
+    int queueId;
+    struct message msg2Send;
+    
+    LOG_ACTION("Creating message queue");
+    key_t queueHashKey = ftok(".", 'A');
+
+    LOG_ACTION("Opening message queue");
+    queueId = msgget(
+        queueHashKey,
+        IPC_CREAT | 0666
+    );
+
+    if (queueId == -1)
+    {
+        LOG_ERROR("Failed to open message queue");
+        exit(EXIT_FAILURE);
+    }
+
+    LOG_SUCCESS("Message queue opened with id %d", queueId);
+
+    LOG_ACTION("Getting data from user");
+
+    while (1)
+    {
+        printf("\tEnter family member name: \n");
+        scanf("%s", msg2Send.text);
+        printf("\tEnter family level: \n");
+        scanf("%ld", &msg2Send.type);
+        if(msgsnd(queueId, &msg2Send, sizeof(msg2Send) - sizeof(long), 0) == -1)
+        {
+            LOG_ERROR("Failed to send message");
+            exit(EXIT_FAILURE);
+        }
+        LOG_SUCCESS("Sent: %s, Level: %ld", msg2Send.text, msg2Send.type);
+    }
+    
+    return 0;
+}
